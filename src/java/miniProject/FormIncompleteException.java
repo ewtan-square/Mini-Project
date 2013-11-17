16fb703934e313c590cb94e28aabd54c77ca1887
ewtan@uwaterloo.ca <edward.tan0426@gmail.com>
2013-11-17 01:15:20 -0500

    updated new user creation pages

---
+++ src/java/miniProject/FormIncompleteException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniProject;
+
+/**
+ *
+ * @author ET
+ */
+public class FormIncompleteException extends Exception {
+    public FormIncompleteException(String message) {
+        super(message);
+    }
+}
