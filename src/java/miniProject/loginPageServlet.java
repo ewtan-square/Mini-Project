bf4e3980effd0c0f84a2bd51d448e18dd20ed699
ewtan@uwaterloo.ca <edward.tan0426@gmail.com>
2013-11-17 23:35:18 -0500

    Updated the login shit again for the 5th time

--- src/java/miniProject/loginPageServlet.java
+++ src/java/miniProject/loginPageServlet.java
@@ -5,7 +5,6 @@
 package miniProject;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
